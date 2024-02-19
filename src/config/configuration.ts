c52ef946a1b3858d22c237fe3b67a141b30d8cd5
Victor Saravia <mavicdevtech@gmail.com>
2024-02-19 01:12:59 -0500

    feat: add @nestjs/swagger

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,17 @@
 import { registerAs } from '@nestjs/config';
+import * as PACKAGE_JSON from '../../package.json';
 
 export const config = registerAs('config', () => ({
+  project: {
+    name: PACKAGE_JSON.name,
+    version: PACKAGE_JSON.version,
+    description: PACKAGE_JSON.description,
+  },
   server: {
     port: parseInt(process.env.PORT, 10) || 3000,
+  },
+  swagger: {
+    enabled: Boolean(process.env.SWAGGER_ENABLED.toLowerCase() === 'true'),
+    path: process.env.SWAGGER_PATH,
   },
 }));
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const { server } =
+  const { server, project, swagger } =
     app.get<ConfigService>(ConfigService)['internalConfig']['config'];
+
+  if (swagger.enabled) {
+    const config = new DocumentBuilder()
+      .setTitle(project.name)
+      .setDescription(project.description)
+      .setVersion(project.version)
+      .build();
+    const document = SwaggerModule.createDocument(app, config, {});
+    SwaggerModule.setup(`/${swagger.path}`, app, document);
+  }
 
   await app.listen(server.port, () => {
     Logger.log(`App running on port : ${server.port}`);
+    swagger.enabled &&
+      Logger.log(
+        `Swagger URL: http://localhost:${server.port}/${swagger.path}`,
+      );
   });
 }
 bootstrap();
